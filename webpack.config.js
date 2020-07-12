6e04f513d34b381c054e56c9dde9eb095bfa3475
m7r-227 <msetair.dedeset@gmail.com>
2020-07-12 14:25:29 +0200

    add env config

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,60 @@
 /* eslint-env node */
 const path = require('path');
 
-module.exports = {
-    entry: {
-        content: './src/content/index.js',
-        background: './src/background/index.js'
-    },
-    devtool: 'source-map',
-    output: {
-        path: path.join(__dirname, 'build'),
-        filename: '[name].js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.handlebars$/i,
-                loader: 'handlebars-loader',
-                options: {
-                    helperDirs: path.join(__dirname, 'src/content/views/helpers')
-                }
-            },
-            {
-                test: /\.(png|jpg|gif|svg)$/i,
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            limit: 1024 * 1024 * 1024,
-                        }
+const Dotenv = require('dotenv-webpack');
+
+module.exports = (env) => {
+    const isProduction = env.NODE_ENV === 'production';
+    const envPath = isProduction ? '.env' : '.env.example';
+
+    return {
+        entry: {
+            content: './src/content/index.js',
+            background: './src/background/index.js'
+        },
+        devtool: 'source-map',
+        output: {
+            path: path.join(__dirname, 'build'),
+            filename: '[name].js'
+        },
+        plugins: [
+            new Dotenv({
+                path: envPath,
+                secure: isProduction
+            })
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.handlebars$/i,
+                    loader: 'handlebars-loader',
+                    options: {
+                        helperDirs: path.join(__dirname, 'src/content/views/helpers')
                     }
-                ]
-            },
-            {
-                test: /\.scss$/i,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
-            }
-        ]
-    },
-    watchOptions: {
-        ignored: /node_modules/
-    }
+                },
+                {
+                    test: /\.(png|jpg|gif|svg)$/i,
+                    use: [
+                        {
+                            loader: 'url-loader',
+                            options: {
+                                limit: 1024 * 1024 * 1024,
+                            }
+                        }
+                    ]
+                },
+                {
+                    test: /\.scss$/i,
+                    use: [
+                        'style-loader',
+                        'css-loader',
+                        'sass-loader'
+                    ]
+                }
+            ]
+        },
+        watchOptions: {
+            ignored: /node_modules/
+        }
+    };
 };
